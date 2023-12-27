a15fb588cd3e3bb65a74f516c25e849b58947fad
Yash patel <73232849+yash-61016@users.noreply.github.com>
2023-12-27 21:01:05 +0000

    Update build configuration and resolve aliases

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,12 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  resolve: {
+    alias: {
+      vue: 'vue/dist/vue.esm-bundler.js',
+      "@": "/src",
+    },
+  },
   plugins: [vue()],
   base: './',
 })
